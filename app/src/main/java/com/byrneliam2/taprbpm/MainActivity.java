8d479d37a1f9304f04a13e11ba2efd743e12a4e6
Liam <liam.jb1@gmail.com>
2018-02-08 21:47:11 +1300

    Gesture listener implementations

--- app/src/main/java/com/byrneliam2/taprbpm/MainActivity.java
+++ app/src/main/java/com/byrneliam2/taprbpm/MainActivity.java
@@ -1,28 +1,32 @@
 package com.byrneliam2.taprbpm;
 
 import android.graphics.Typeface;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+    GestureDetector.OnGestureListener {
+
+    private GestureDetectorCompat gd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gd = new GestureDetectorCompat(this, this);
+
         setTextViewFonts();
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-
         // From https://developer.android.com/training/system-ui/immersive.html
         if (hasFocus) {
             getWindow().getDecorView().setSystemUiVisibility(
@@ -37,8 +41,39 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        Toast.makeText(getApplicationContext(), "Touch!", Toast.LENGTH_SHORT).show();
-        return super.onTouchEvent(event);
+        return gd.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        if (motionEvent.getAction() != MotionEvent.ACTION_DOWN) return false;
+        Toast.makeText(this, "Touch!", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+        Toast.makeText(this, "Long touch!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
     }
 
     private void setTextViewFonts() {
